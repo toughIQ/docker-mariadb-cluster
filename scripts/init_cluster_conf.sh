ec12f9151d1a6b682a9c504e976ccf2f94a3a24b
toughIQ <toughiq@gmail.com>
2016-08-31 12:40:18 +0200

    get rancher service name

--- scripts/init_cluster_conf.sh
+++ scripts/init_cluster_conf.sh
@@ -8,7 +8,7 @@ CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=no"
 
 # RANCHER_STACK variable is needed to get correct hostname settings for replication
 RANCHER_STACK=`curl http://rancher-metadata/latest/self/service/stack_name`
-
+MY_NAME=`curl http://rancher-metadata/latest/self/service/name`
 
 # we create a galera config
 config_file="/etc/mysql/conf.d/galera.cnf"
@@ -17,16 +17,16 @@ cat <<EOF > $config_file
 # Node specifics 
 [mysqld] 
 # enabled for rancher testing
-wsrep-node-name = $HOSTNAME.$RANCHER_STACK 
-wsrep-sst-receive-address = $HOSTNAME.$RANCHER_STACK
-wsrep-node-incoming-address = $HOSTNAME.$RANCHER_STACK
+wsrep-node-name = $MY_NAME.$RANCHER_STACK 
+wsrep-sst-receive-address = $MY_NAME.$RANCHER_STACK
+wsrep-node-incoming-address = $MY_NAME.$RANCHER_STACK
 
 # Cluster settings
 wsrep-on=ON
 wsrep-cluster-name = "$CLUSTER_NAME" 
 wsrep-cluster-address = $CLUSTER_ADDRESS
 wsrep-provider = /usr/lib/galera/libgalera_smm.so 
-wsrep-provider-options = "gcache.size=256M;gcache.page_size=128M;debug=yes$MCAST_OPTION" 
+wsrep-provider-options = "gcache.size=256M;gcache.page_size=128M;debug=no" 
 wsrep-sst-auth = "$GALERA_USER:$GALERA_PASS" 
 wsrep_sst_method = rsync
 binlog-format = row 
